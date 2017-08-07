460de43a269e6389534d031d13883e15038e95f9
Martin Hochel <hochelmartin@gmail.com>
2017-08-07 19:07:39 +0200

    fix: make project compile without errors for both unit and e2e

--- e2e/test.ts
+++ e2e/test.ts
@@ -1,8 +1,20 @@
-import { Selector } from 'testcafe';
+import { Selector } from 'testcafe'
 
-fixture `TestCafe fixture`
-    .page `http://example.com`;
+import { User } from '../src/types'
+
+fixture`TestCafe fixture`.page`http://example.com`
 
 test('TestCafe test', async t => {
-    await t.wait(1000);
-});
+  await t.wait(1000)
+})
+
+test(`should be a user like Jon Snow`, async t => {
+  const user: User = {
+    age: 30,
+    email: 'jon@snow.got',
+    name: 'Jon',
+    hobbies: ['dying', 'arguing with khalesi', 'knowing nothing'],
+  }
+
+  await t.wait(1000)
+})
--- unit/test.ts
+++ unit/test.ts
@@ -1,8 +1,17 @@
-describe('Mocha', () => {
-    it('Mocha test', () => {
-        var a = false;
+import { User } from '../src/types'
 
-        if(a)
-            throw new Error('Never fails');
-    });
-});
+describe('Mocha', () => {
+  it('Mocha test', () => {
+    var a = false
+
+    if (a) throw new Error('Never fails')
+  })
+  it(`should be a user like Jon Snow`, () => {
+    const user: User = {
+      age: 30,
+      email: 'jon@snow.got',
+      name: 'Jon',
+      hobbies: ['dying', 'arguing with khalesi', 'knowing nothing'],
+    }
+  })
+})
